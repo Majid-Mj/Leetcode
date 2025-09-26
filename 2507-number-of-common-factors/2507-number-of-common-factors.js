04847fd5a601a1bfa8dfdbb521dc18e2cae3df12
Abdul Majid <majidvp501@gmail.com>
2025-09-26 20:28:47 +0530

    Time: 0 ms (100%), Space: 54.2 MB (43.21%) - LeetHub

---
+++ 2507-number-of-common-factors/2507-number-of-common-factors.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function(a, b) {
+    let count=0;
+    let limit;
+    if(a <= b){
+        limit = a;
+    }else if(a >= b){
+        limit = b;
+    }
+    for(i=1; i<=limit;  i++){
+        if(a % i === 0 && b % i === 0){
+            count++
+        }
+    }
+    return count;
+    
+};
