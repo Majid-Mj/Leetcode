119bc1b611b118f3fc4568075369cf4950bf095c
Abdul Majid <majidvp501@gmail.com>
2025-10-03 18:31:40 +0530

    Time: 45 ms (44.44%), Space: 53.8 MB (35.97%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.js
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.js
@@ -3,13 +3,5 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    let result ="";
-    for(let x of address){
-        if(x === "."){
-            result += "[.]"
-        }else{
-            result += x;
-        }
-    }
-    return result;
+    return address.replaceAll(".","[.]")
 };
