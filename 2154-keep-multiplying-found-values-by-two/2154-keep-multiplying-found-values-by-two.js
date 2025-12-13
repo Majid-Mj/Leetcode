6650ddae4a3d24e915d87b34119da133aeb59cef
Abdul Majid <majidvp501@gmail.com>
2025-12-13 12:35:29 +0530

    Time: 0 ms (100%), Space: 55.4 MB (63.89%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+var findFinalValue = function(nums, original) {
+   for(let i=0; i<nums.length; i++){
+    if(nums.includes(original)){
+        original = 2 * original
+    }else{
+        break;
+    }
+   }
+    return original;
+};
