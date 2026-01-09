46f483134dae4fcbd5add4ff3181fcf0a28a351e
Abdul Majid <majidvp501@gmail.com>
2026-01-09 09:57:42 +0530

    Time: 8 ms (21.07%), Space: 43.8 MB (70.33%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int FindFinalValue(int[] nums, int original) 
+    {
+        Array.Sort(nums);
+        foreach(int num in nums){
+            if(original == num){
+                original *= 2;
+            }
+        }
+        return original;
+    }
+}
